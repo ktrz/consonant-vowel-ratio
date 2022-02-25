a8d71faf0faab049ee7e9e9df867b00e68fda122
Chris Trzesniewski <k.trzesniewski@gmail.com>
2022-02-25 13:49:44 +0100

    feat: update previous comment
    gibberish

--- src/index.ts
+++ src/index.ts
@@ -73,8 +73,17 @@ const postComment = async (comment: string) => {
   if (token && pullRequest) {
     const octokit = getOctokit(token);
 
-    const previousComments = await getPreviousComment() || [];
-    previousComments.sort((a, b) => new Date(a.submitted_at!) < new Date(b.submitted_at!) ? 1 : -1)
+    const previousComments = (await getPreviousComment()) || [];
+    previousComments.sort((a, b) => (new Date(a.submitted_at!) < new Date(b.submitted_at!) ? 1 : -1));
+
+    // some gibberish
+    // sdfghlwsgfd
+    // sdfghlwsgfd
+    // sdfghlwsgfd
+    // sdfghlwsgfd
+    // sdfghlwsgfd
+    // sdfghlwsgfd
+
     if (previousComments.length) {
       await octokit.rest.pulls.updateReview({
         review_id: previousComments[0].id,
