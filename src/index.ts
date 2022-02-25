4823965ce7403b35f96ca4c3b861a107fd63b37e
Chris Trzesniewski <k.trzesniewski@gmail.com>
2022-02-25 09:30:04 +0100

    feat: add @actions packages
    * Greet based on action inputs

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
-function greeting(name: string) {
-  console.log(`Hello ${name}`)
+import { getInput } from '@actions/core'
+import { context } from '@actions/github'
+
+function greeting(name: string, repo: string) {
+  console.log(`Hello ${name}! You are running a GitHub action in ${repo}`)
 }
 
-greeting('World')
+const inputName = getInput('name')
+
+greeting(inputName, context.repo.repo)
