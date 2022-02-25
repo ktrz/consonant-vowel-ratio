7f2fd35d0aeb5cc9dd26c32dca616950d505fc52
Chris Trzesniewski <k.trzesniewski@gmail.com>
2022-02-25 09:46:34 +0100

    feat: add @actions packages
    * print action info

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,21 @@ const getRepoUrl = ({ repo, serverUrl }: GithubContext): string => {
 
 greeting(inputName, getRepoUrl(context))
 
-console.log("--- Payload ---")
+console.log('--- Action Info ---')
+console.log('eventName:', context.eventName)
+console.log('sha:', context.sha)
+console.log('ref:', context.ref)
+console.log('workflow:', context.workflow)
+console.log('action:', context.action)
+console.log('actor:', context.actor)
+console.log('job:', context.job)
+console.log('runNumber:', context.runNumber)
+console.log('runId:', context.runId)
+console.log('apiUrl:', context.apiUrl)
+console.log('serverUrl:', context.serverUrl)
+console.log('graphqlUrl:', context.graphqlUrl)
+console.log('-------------------')
+
+console.log('--- Payload ---')
 console.log(JSON.stringify(context.payload, undefined, 2))
-console.log("---------------")
+console.log('---------------')
